298d71afa5b4be57561580ae2933388147ba2134
Liam <liammorley123@live.co.uk>
2021-01-15 09:59:13 +0000

    start task, get data and add start button

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
 // fetch api data: 10 random questions
-// ask for username input 
+
+async function fetchData() {
+  let response = await fetch(
+    "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=boolean"
+  );
+  let data = await response.json();
+  //console.log(data);
+}
+
+// ask for username input
+
+let username = prompt("What is your name?");
+
 // set up start button- "start";
+
+let startButton = document.createElement("button");
+startButton.innerText = "Start";
+document.body.appendChild(startButton);
+
 // when the button is clicked, pull the fetch request
+
+startButton.addEventListener("click", fetchData);
+
 // display a question
 // display two other buttons - true / false
 // set score to zero
-// if whatever is pressed matches answer - show green tick & plus 1 score 
+// if whatever is pressed matches answer - show green tick & plus 1 score
 // display another question
 // repeat process (*10)
-// then show final score - with username 
+// then show final score - with username
 // display something funny - dad jokes fetch api
 //
+
+/// stretch
+// Add difficulty levels
+// add stored usernames for leaderboard
