6ed027681e794c26e378074ca8309c19f41b0965
Stina <christinama92@gmail.com>
2021-01-15 09:50:14 +0000

    plan

---
+++ main.js
@@ -0,0 +1,13 @@
+// fetch api data: 10 random questions
+// ask for username input 
+// set up start button- "start";
+// when the button is clicked, pull the fetch request
+// display a question
+// display two other buttons - true / false
+// set score to zero
+// if whatever is pressed matches answer - show green tick & plus 1 score 
+// display another question
+// repeat process (*10)
+// then show final score - with username 
+// display something funny - dad jokes fetch api
+//
